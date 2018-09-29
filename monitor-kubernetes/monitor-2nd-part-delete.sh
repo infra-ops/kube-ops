a05d96f08b362672555894d41d5acd795a556698
nik786 <chakraborty.rock@gmail.com>
2018-09-29 16:22:48 +0530

    changes

--- monitor-kubernetes/monitor-2nd-part-delete.sh
+++
@@ -1,9 +0,0 @@
-kubectl delete  -f kube-state/kube-state-metrics-clusterRole.yaml
-kubectl delete  -f kube-state/kube-state-metrics-clusterRoleBinding.yaml
-kubectl delete  -f kube-state/kube-state-metrics-role.yaml
-kubectl delete  -f kube-state/kube-state-metrics-roleBinding.yaml
-kubectl delete  -f kube-state/kube-state-metrics-serviceAccount.yaml
-kubectl delete  -f kube-state/kube-state-metrics-deployment.yaml
-kubectl delete  -f kube-state/kube-state-metrics-serviceMonitor.yaml
-kubectl delete  -f kube-state/kube-state-metrics-service.yaml
-
