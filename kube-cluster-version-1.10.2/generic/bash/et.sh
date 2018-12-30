87ab7abf65ee2c1ef4bab4859136b8b0ebd85726
nik786 <chakraborty.rock@gmail.com>
2018-12-30 11:23:28 +0530

    Update et.sh

--- kube-cluster-version-1.10.2/generic/bash/et.sh
+++ kube-cluster-version-1.10.2/generic/bash/et.sh
@@ -4,4 +4,14 @@ etcdctl ls /calico/v1/ipam/v4/pool/
 
 etcdctl get /calico/v1/ipam/v4/pool/192.168.0.0-16
 
-etcdctl cluster-health
+./etcdctl cluster-health
+
+./etcdctl member list
+
+etcdctl backup --data-dir /var/lib/etcd --backup-dir /var/lib/etcd_backup
+
+ chown etcd -R /var/lib/etcd
+ 
+ etcdctl member add demo-etcd-2 http://10.240.0.2:2380
+ 
+ etcdctl member remove 1609b5a3a078c227
